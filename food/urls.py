0f04cc1a3dfb15d6cb6c9e11e46e86ca9ae5fd46
麻宏略 <ksy-walry@mahonglvedeMacBook-Pro.local>
2017-10-20 17:08:10 +0800

    change

---
+++ food/urls.py
@@ -0,0 +1,20 @@
+#encoding: utf-8
+
+
+from django.conf.urls import url
+from food import views
+
+# from .views import RegisterView, ActiveView, \
+#                     LoginView, LogoutView, \
+#                     ResetPasswordView, ResetPasswordConfirmView, \
+#                     ModifyPasswordView, ChangePasswordView, \
+#                     UserExtBaseView, UserExtAvatarView,\
+#                     UserAddressListView, UserAddressCreateView, UserAddressDeleteView, UserAddressUpdateView, \
+#                     TestView
+
+app_name = 'food'
+
+urlpatterns = [
+    url(r'^test/', views.test, name="test"),
+    url(r'^test1/', views.test1, name="test1"),
+]
--- food/views.py
+++ food/views.py
@@ -1,6 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import logging, json
 
 # Create your views here.
+logger = logging.getLogger("mahl")
+logging.basicConfig(level=logging.DEBUG,format="﻿%(name)s ﻿%(asctime)s ﻿%（message)s",\
+                    filename="walry.log")
+
+@csrf_exempt
+def test(request):
+    logging.debug("request:{}".format(request.COOKIES))
+    response = HttpResponse(content_type='application/json')
+    name = response.set_cookie("name","mhl")
+    # response.content = json.dumps(status)
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@csrf_exempt
+def test1(request):
+    logging.debug("request_test1:{}".format(request.COOKIES))
+    response = HttpResponse(content_type='application/json')
+    # name = response.set_cookie("name","mhl")
+    # response.content = json.dumps(status)
+    response["Access-Control-Allow-Origin"] = "*"
+    return response
